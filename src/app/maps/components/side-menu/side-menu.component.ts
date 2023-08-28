ca2145cedabe376c401effc4ab0976b9636d77cd
MarkedOliv <olivdev4@gmail.com>
2023-08-28 14:09:39 -0400

    finalización de el sidemenu

---
+++ src/app/maps/components/side-menu/side-menu.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { MenuItem } from '../../interfaces/menu-item.interface';
+
+@Component({
+  selector: 'maps-side-menu',
+  templateUrl: './side-menu.component.html',
+  styleUrls: ['./side-menu.component.css']
+})
+export class SideMenuComponent {
+
+  public menuItems: MenuItem[] = [
+    {
+      route: '/maps/fullscreen',
+      name: 'FullScreen',
+    },
+    {
+      route: '/maps/zoom',
+      name: 'Zoom',
+    },
+    {
+      route: '/maps/markers',
+      name: 'Markers',
+    },
+    {
+      route: '/maps/properties',
+      name: 'Properties',
+    },
+  ]
+}
