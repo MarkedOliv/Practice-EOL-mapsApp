58ad6a29b0932ceb5584b5427580b7f42922c709
MarkedOliv <olivdev4@gmail.com>
2023-08-28 14:12:44 -0400

    finalización de la página de markers con su interfaz

---
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -0,0 +1,107 @@
+import { Component, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
+
+import { LngLat, Map, Marker } from 'mapbox-gl';
+
+import {
+  MarkerAndColor,
+  PlainMarker,
+} from '../../interfaces/markers.interface';
+
+@Component({
+  templateUrl: './markers-page.component.html',
+  styleUrls: ['./markers-page.component.css'],
+})
+export class MarkersPageComponent implements AfterViewInit {
+  @ViewChild('map')
+  public divMap?: ElementRef;
+
+  public markers: MarkerAndColor[] = [];
+
+  public map?: Map;
+  public currentLngLat?: LngLat = new LngLat(
+    -69.24283378562427,
+    9.528983271080165
+  );
+
+  ngAfterViewInit(): void {
+    if (!this.divMap) throw 'El elemento HTML no fue encontrado';
+
+    this.map = new Map({
+      container: this.divMap.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: this.currentLngLat,
+      zoom: 13,
+    });
+
+    this.readFromLocalStorage();
+  }
+
+  createMarker() {
+    if (!this.map) return;
+    const color = '#xxxxxx'.replace(/x/g, (y) =>
+      ((Math.random() * 16) | 0).toString(16)
+    );
+    const lngLat = this.map.getCenter();
+
+    this.addMarker(lngLat, color);
+  }
+
+  addMarker(lngLat: LngLat, color: string) {
+    if (!this.map) return;
+
+    const marker = new Marker({
+      color: color,
+      draggable: true,
+    })
+      .setLngLat(lngLat)
+      .addTo(this.map);
+
+    this.markers.push({
+      color,
+      marker,
+    });
+
+    this.saveToLocalStorage();
+
+    marker.on('dragend', () => this.saveToLocalStorage());
+  }
+
+  deleteMarker(index: number) {
+    this.markers[index].marker.remove();
+    this.markers.splice(index, 1);
+
+    this.saveToLocalStorage();
+  }
+
+  flyTo(marker: Marker) {
+    this.map?.flyTo({
+      zoom: 14,
+      center: marker.getLngLat(),
+    });
+  }
+
+  saveToLocalStorage() {
+    const plainMarkers: PlainMarker[] = this.markers.map(
+      ({ color, marker }) => {
+        return {
+          color,
+          lngLat: marker.getLngLat().toArray(),
+        };
+      }
+    );
+
+    localStorage.setItem('plainMarkers', JSON.stringify(plainMarkers));
+  }
+
+  readFromLocalStorage() {
+    const plainMarkersString = localStorage.getItem('plainMarkers') ?? '[]';
+    const plainMarkers: PlainMarker[] = JSON.parse(plainMarkersString);
+
+    plainMarkers.forEach(({ color, lngLat }) => {
+      const [lng, lat] = lngLat;
+      const coords = new LngLat(lng, lat);
+
+      this.addMarker(coords, color);
+    });
+  }
+}
